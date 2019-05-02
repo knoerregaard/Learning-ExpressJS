2300be9e6ef86e1f5aeb67c63146fa5782c6ceef
Klaus Nørregaard <kl@eadania.dk>
2019-05-02 12:10:58 +0200

    update

--- 02_server.js
+++ 02_server.js
@@ -1,33 +1,28 @@
 /*
- * Subject : ExpressJS Server
- * Source  : https://expressjs.com/
- * This server is written in JavaScript
- * The server is responsible for 
- * - Setup middleware
- * - handle incomming request from clients
- * - handle database communication
- * - manipulate data when nessecary
- * - initiating the server
- * Change <username>, <password>, <database> and <collection> where needed.
+ * Subject : ExpressJS Server, ExpressJS router
+ * Source  : https://expressjs.com/en/guide/routing.html
+ * 
+ * This is an addition to 01_server.js
+ * A server application can have lots of endpoints - maybe hundreds of endpoints.
+ * To structure your server better, you can do group you requesthandlers into
+ * categories and seperate files using ExpressJS/Router.
  */
 
 const express = require('express')
 const app = express()
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL = 'mongodb+srv://<username>:<password>@cluster0-cpb0x.mongodb.net/<database>?retryWrites=true';
+const MONGO_URL = 'mongodb+srv://mongouser:mongo123@cluster0-cpb0x.mongodb.net/test?retryWrites=true';
 
 let db = "";
 
 MongoClient.connect(MONGO_URL, function (err, client) {
   if (err) throw err;
-  db = client.db('');
+  db = client.db('test');
 }); 
 
-  
 // process.env.PORT is relevant when you use Heroku as a host.
 const PORT = 3000 |  process.env.PORT;
-
 
 //---- Setting up middleware START ----//
 
@@ -54,9 +49,10 @@ app.get('/', (req, res) => {
     //cats is a collection in the database
     db.collection('cats').findOne({kattenavn: "mis"}, function (findErr, result) {
         if (findErr) throw findErr;
-        console.log(result.name);
+        console.log(result);
+        
+        res.status(200).send(result)
       });
-    res.status(200).send(result)
 });
 
 app.get('/test/:name/:id', (req, res)=>{
